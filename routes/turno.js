d248980b51b66322f6b395827db440dd0084c399
rochyscarlata <rosarioscarlata24@gmail.com>
2020-07-03 12:30:48 -0300

    agregando para turnos admin

--- routes/turno.js
+++ routes/turno.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const turnoController = require("../controller/turnoController")
 
 router.route("/")
+        .get(turnoController.mostrarTurnos)
         .post(turnoController.cargarTurnos);
 router.route("/:_id")
         .get(turnoController.mostrarTurnosId)
